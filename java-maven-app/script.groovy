2749e158b93b933f6e7b7ed8a2e912f872bf0928
lala-la-flaca <78053652+lala-la-flaca@users.noreply.github.com>
2025-03-16 13:48:53 -0500

    Update script.groovy

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,14 +1,25 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application..."
+    dir('java-maven-app') {
+        sh 'mvn package'
+        }
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+    echo "building the docker Image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]){
+        sh """
+            cd java-maven-app/
+            docker build -t lala011/demo-app:jma-2.0 .
+            echo $PWD | docker login -u $USER --password-stdin
+            docker push lala011/demo-app:jma-2.0
+        """                               
+    }    
 }
+
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "Deploying version ${params.VERSION}"
 }
 
 return this
