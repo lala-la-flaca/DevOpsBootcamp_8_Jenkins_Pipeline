9159cc3eb29a10b266bf0ceedcf4382f716059c6
Unicorn <lala.la.flaca11@gmail.com>
2025-03-18 06:21:24 -0500

    Modifying jenins file and grrovy script to use sharedlibrary

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -4,25 +4,6 @@ def testApp() {
 
 
 
-def buildJar() {
-    echo "building the application..."
-    dir('java-maven-app') {
-        sh 'mvn package'
-        }
-}
-
-def buildImage() {
-    echo "building the docker Image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]){
-        sh """
-            cd java-maven-app/
-            docker build -t lala011/demo-app:jma-2.0 .
-            echo $PWD | docker login -u $USER --password-stdin
-            docker push lala011/demo-app:jma-2.0
-        """                               
-    }    
-}
-
 
 def deployApp() {
     echo 'deploying the application...'
